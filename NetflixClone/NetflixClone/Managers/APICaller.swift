a992be2ff879b5bf9c5ab63685f3845e3340f534
Abhishek Dhok <abhishekdhok96@gmail.com>
2023-08-19 22:56:56 +0530

    Added code to fetch the API response from TMDB

---
+++ NetflixClone/NetflixClone/Managers/APICaller.swift
@@ -0,0 +1,41 @@
+//
+//  APICaller.swift
+//  NetflixClone
+//
+//  Created by Abhishek Dilip Dhok on 19/08/23.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "d749354f8a16dfe0306561c315c44550"
+    static let baseURL = "https://api.themoviedb.org"
+}
+
+enum APIError: Error {
+    case failedToGetData
+}
+
+class APICaller {
+    static let shared = APICaller()
+
+    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
+        guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+
+            do {
+                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                completion(.success(results.results))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+
+        task.resume()
+    }
+}
---
+++ NetflixClone/NetflixClone/Models/Movie.swift
@@ -0,0 +1,24 @@
+//
+//  Movie.swift
+//  NetflixClone
+//
+//  Created by Abhishek Dilip Dhok on 19/08/23.
+//
+
+import Foundation
+
+struct TrendingMoviesResponse: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable {
+    let id: Int
+    let media_type: String?
+    let originalName: String?
+    let original_title: String?
+    let poster_path: String?
+    let overview: String?
+    let vote_count: Int
+    let release_date: String?
+    let vote_average: Double
+}
